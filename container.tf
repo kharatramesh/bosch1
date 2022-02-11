4fffe63984d63718034b439d4717d1eaa0e84806
Ramesh KHARAT <62638281+kharatramesh@users.noreply.github.com>
2022-02-11 12:50:23 +0530

    Create container.tf

---
+++ container.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_container_group" "example" {
+  name                = "example-continst"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  ip_address_type     = "public"
+  dns_name_label      = "priar-label"
+  os_type             = "Linux"
+
+  container {
+    name   = "hello-world1"
+    image  = "docker.io/httpd:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  tags = {
+    environment = "testing"
+  }
+}
